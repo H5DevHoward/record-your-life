aeb8cd13c352e9188dbc95aaaf165cca68876287
H5DevHoward <h5devhoward@gmail.com>
2017-07-18 10:25:23 +0800

    initial commit
    restful api 编辑

---
+++ README.md
@@ -0,0 +1,23 @@
+# Skeleton project for Swagger
+
+> ## step1.
+
+```Shell
+$ sudo npm i swagger -g
+```
+
+> ## step2.
+
+```Shell
+$ npm i
+```
+
+> ## step3.
+
+### *for api dev and test*
+
+```Shell
+$ swagger project start
+# 再新建一个命令行窗口
+$ swagger project edit
+```
---
+++ test/api/controllers/movie_get.js
@@ -0,0 +1,48 @@
+var should = require('should');
+var request = require('supertest');
+var server = require('../../../app');
+
+describe('controllers', function() {
+
+  describe('movie_get', function() {
+
+    describe('GET /movie', function() {
+
+      it('should return a default string', function(done) {
+
+        request(server)
+          .get('/movie')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+
+            res.body.should.eql('Hello, stranger!');
+
+            done();
+          });
+      });
+
+      it('should accept a name parameter', function(done) {
+
+        request(server)
+          .get('/movie')
+          .query({ name: 'Scott'})
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+
+            res.body.should.eql('Hello, Scott!');
+
+            done();
+          });
+      });
+
+    });
+
+  });
+
+});
